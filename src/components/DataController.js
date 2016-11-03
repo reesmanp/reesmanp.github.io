19c5127fb1a3b0a04c7b27b50c088d0824c3cf8b
Paul Reesman <paul.reesman@seattlechildrens.org>
2016-11-03 15:19:34 -0700

    React-materialize Accordion is broken currently. Applied non-accordion yet expandable fix.

--- src/components/DataController.js
+++ src/components/DataController.js
@@ -2,6 +2,7 @@ import GetData_Text from '../data/TextData';
 import GetData_NestedText from '../data/TextData_Nested';
 import GetData_PDF from '../data/PDFData';
 import GetData_Contact from '../data/ContactData';
+import GetData_Accordion from '../data/TextData_Accordion';
 const React = require('react');
 
 /// Controller to pass the navigated data to the correct component in order to render it properly
@@ -13,12 +14,13 @@ module.exports = React.createClass({
                 return(<GetData_Text selectedKey={this.props.selectedKey} selectedObj={this.props.selectedObj}/>);
             case "Portfolio":
             case "Tools I Use":
-            case "Blog":
                 return(<GetData_NestedText selectedKey={this.props.selectedKey} selectedObj={this.props.selectedObj}/>);
             case "Resume":
                 return(<GetData_PDF/>);
             case "Contact Me":
                 return(<GetData_Contact selectedKey={this.props.selectedKey} selectedObj={this.props.selectedObj}/>);
+            case "Blog":
+                return(<GetData_Accordion selectedKey={this.props.selectedKey} selectedObj={this.props.selectedObj}/>);
             default:
                 return(<div/>)
         }
---
+++ src/data/TextData_Accordion.js
@@ -0,0 +1,20 @@
+import { Collapsible, CollapsibleItem } from 'react-materialize';
+import GetData_Text from './TextData';
+const React = require('react');
+
+/// Populates the data rendered when Navigation bar is clicked --- for nested text
+var GetData_Accordion = React.createClass({
+  render: function () {
+    return(
+      <Collapsible accordion popout defaultActiveKey={-1}>
+        {Object.keys(this.props.selectedObj).map( (key,idx) =>
+          <CollapsibleItem header={key} eventKey={idx}>
+            <GetData_Text selectedKey={key} selectedObj={this.props.selectedObj[key]}/>
+          </CollapsibleItem>
+        )}
+      </Collapsible>
+    )
+  }
+});
+
+module.exports = GetData_Accordion;
